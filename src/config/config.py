d93dd251309e40cd0244e4c851521c64caa94e17
Kariuki Antony <antonygichoya9@gmail.com>
2023-11-13 12:40:14 +0300

    change expire token time

--- src/config/config.py
+++ src/config/config.py
@@ -9,8 +9,8 @@ class DevConfig(Config):
      SQLALCHEMY_TRACK_MODIFICATION = False
      DEBUG = True
      JWT_SECRET_KEY = config("JWT_SECRET_KEY") 
-     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
-     JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=15)
+     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=2)
+     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=2)
      CORS_HEADERS = "Content-Type"
 
 
